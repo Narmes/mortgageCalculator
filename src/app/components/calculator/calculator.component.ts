34a68ed137904a64e2dd843cf77fd347ec71bcbf
Narmes <@horux.08>
2023-09-20 23:08:23 -0500

    all-app

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor() { }
+  public dataC:FormGroup;
+  public EstimatedAmount='00.0';
+  public LoanAmount='00.0';
+
+  constructor(
+    private fb:FormBuilder
+  ) {
+    this.dataC = this.fb.group({
+      purchasePrice:['',Validators.required],
+      downPayment:['',Validators.required],
+      interestRate:['',Validators.required],
+      repaymentTime:['',[Validators.required,Validators.pattern("[0-9]+")]],
+    });
+   }
 
   ngOnInit(): void {
+  }
+
+  mortagageQuote(){
+    var p = (this.dataC.value.purchasePrice - this.dataC.value.downPayment);
+    var r = ((this.dataC.value.interestRate/100)/12);
+    var n = (this.dataC.value.repaymentTime*12);
+
+    var A = r*((1+r)**n);
+    var B = ((1+r)**n)-1;
+    var m=p*(A/B);
+
+    this.LoanAmount = ''+p;
+
+    this.EstimatedAmount = ''+m;
   }
 
 }
